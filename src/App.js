75c9acc838965f967872035013427a959ada743a
rajkishorepatra <rpatrasm@gmail.com>
2023-08-18 11:21:48 -0700

    bg & contents

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React,{useState, useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import Carousel from 'react-bootstrap/Carousel';
 import bg1 from './img/bg1.jpg'
-import bg2 from './img/bg2.jpg'
-import bg3 from './img/bg3.jpg'
-import bg4 from './img/bg4.jpg'
+// import bg2 from './img/bg2.jpg'
+// import bg3 from './img/bg3.jpg'
+// import bg4 from './img/bg4.jpg'
 import './App.css'
 import { Button } from 'react-bootstrap';
 
@@ -33,29 +33,12 @@ function App() {
   return (
     <>
       <div className='home'>
-        <Carousel fade controls={false} keyboard={true} touch>
+        <Carousel fade indicators={false} controls={false} keyboard={true} touch>
           <Carousel.Item>
             <img src={bg1} alt='bg1' />
-            <Carousel.Caption className='my-3'>
+            {/* <Carousel.Caption className='my-3'>
               <Button variant='danger' className='px-5 py-2 h1' href='https://www.youtube.com'>Watch Trailer</Button>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img src={bg2} alt='bg2' />
-            <Carousel.Caption className='my-3'>
-              <Button variant='danger' className='px-5 py-2 h1' href='https://www.youtube.com'>Watch Trailer</Button>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img src={bg3} alt='bg3' />
-            <Carousel.Caption className='my-3'>
-              <Button variant='danger' className='px-5 py-2 h1' href='https://www.youtube.com'>Watch Trailer</Button>
-            </Carousel.Caption>
-          </Carousel.Item><Carousel.Item>
-            <img src={bg4} alt='bg4' />
-            <Carousel.Caption className='my-3'>
-              <Button variant='danger' className='px-5 py-2 h1' href='https://www.youtube.com'>Watch Trailer</Button>
-            </Carousel.Caption>
+            </Carousel.Caption> */}
           </Carousel.Item>
         </Carousel>
       </div>
@@ -63,7 +46,7 @@ function App() {
 
 
       <div className='register text-center p-1'>
-        <h1 className='px-4'>Register for Lucky Draw Contest</h1>
+        <h1 className='px-4'>Get Your Sweet Housewife a Gift</h1>
         <div className='d-block'>
           <form onSubmit={handleSubmit} method='post' ref={formRef} name="google-sheet">
 
@@ -86,23 +69,21 @@ function App() {
               </label>
             </div>
 
-            <Button variant='none' className='px-5 py-2 h1' type='submit'>Submit</Button>
+            <Button variant='none' className='px-5 py-2 h1 mt-3 mb-4' type='submit'>Submit</Button>
           </form>
         </div>
       </div>
 
       <div className='rules text-center p-5'>
         <h1>Rules & Regulations</h1>
-        <Button variant='danger' className='px-5 py-2 h1 mt-5'>Click Here</Button>
+        <Button style={{backgroundColor:"#2f2a19", color:"white"}} variant='none' className='px-5 py-2 h1 mt-5'>Click Here</Button>
       </div>
-      {/* <div className='footer text-center text-white'>
-        <p>A Project by <a href=''>LEAFLETS</a> (A UNIT OF IAMPIX TECHNOLOGY)</p>
-      </div> */}
+      
       <div
         class="text-center p-3"
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        style={{ background: "linear-gradient(to right, #abbaab, #ffffff)" }}
       >
-        A Project by <a href='https://www.instagram.com/leaflets_app/' style={{ textDecoration: "none" }}>LEAFLETS</a> (A UNIT OF IAMPIX TECHNOLOGY)
+        <h4>A Project by <a href='https://www.instagram.com/we_hypeup/' style={{ textDecoration: "none", color:"purple" }}>Hype Up</a></h4>
       </div>
     </>
   )
